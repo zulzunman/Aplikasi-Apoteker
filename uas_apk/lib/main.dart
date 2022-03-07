0edef1a71a144176eaedcd8173f200ac19588b17
Faizulhaq F.N <101050815+zulzunman@users.noreply.github.com>
2022-03-07 03:22:40 +0000

    Add files via upload

---
+++ uas_apk/lib/main.dart
@@ -0,0 +1,20 @@
+//kode utama Aplikasi tampilan awal
+import 'package:flutter/material.dart';
+import 'package:uas_apk/ui/home.dart';
+
+//package letak folder Anda
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //judul
+      title: 'Tambahkan Daftar',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Home(),
+    );
+  }
+}
